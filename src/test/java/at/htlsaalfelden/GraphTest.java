bca24d2fe9ad89b7268c6f844170bdef4e72bff7
Noah <82201000+Nopro263@users.noreply.github.com>
2025-04-30 10:51:28 +0200

    added dijkstra tests

--- src/test/java/at/htlsaalfelden/GraphTest.java
+++ src/test/java/at/htlsaalfelden/GraphTest.java
@@ -3,6 +3,7 @@ package at.htlsaalfelden;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.function.Consumer;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -119,5 +120,106 @@ class GraphTest {
         graph.dfs(integerVertex -> sb.append(integerVertex.getData()), 1);
 
         assertEquals("12563784910", sb.toString());
+    }
+
+    @Test
+    void dijkstra() {
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
+        graph.addVertex(7);
+        graph.addVertex(8);
+        graph.addVertex(9);
+        graph.addVertex(10);
+
+        graph.connect(1, 2,100);
+        graph.connect(1, 3,540);
+        graph.connect(1, 4,365);
+
+        graph.connect(2, 5,542);
+        graph.connect(2, 6,520);
+
+        graph.connect(3, 7,657);
+        graph.connect(3, 8,126);
+
+        graph.connect(4, 9,80);
+        graph.connect(4, 10,905);
+        graph.connect(1,10,1271);
+
+        int START = 1;
+        int END = 10;
+
+        assertEquals("1->4->10", graph.pathToString(graph.aStar(START, END), graph.getVertex(START)));
+    }
+
+    @Test
+    void dijkstraNoPath() {
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
+        graph.addVertex(7);
+        graph.addVertex(8);
+        graph.addVertex(9);
+        graph.addVertex(10);
+
+        graph.connect(1, 2,100);
+        graph.connect(1, 3,540);
+        graph.connect(1, 4,365);
+
+        graph.connect(2, 5,542);
+        graph.connect(2, 6,520);
+
+        graph.connect(3, 7,657);
+        graph.connect(3, 8,126);
+
+        graph.connect(4, 9,80);
+        graph.connect(4, 10,905);
+        graph.connect(1,10,1271);
+
+        int START = 10;
+        int END = 1;
+
+        assertEquals("No path found!", graph.pathToString(graph.aStar(START, END), graph.getVertex(START)));
+    }
+
+    @Test
+    void dijkstraOmnidirectional() {
+        graph.setOmnidirectional(true);
+
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
+        graph.addVertex(7);
+        graph.addVertex(8);
+        graph.addVertex(9);
+        graph.addVertex(10);
+
+        graph.connect(1, 2,100);
+        graph.connect(1, 3,540);
+        graph.connect(1, 4,365);
+
+        graph.connect(2, 5,542);
+        graph.connect(2, 6,520);
+
+        graph.connect(3, 7,657);
+        graph.connect(3, 8,126);
+
+        graph.connect(4, 9,80);
+        graph.connect(4, 10,905);
+        graph.connect(1,10,1271);
+
+        int START = 10;
+        int END = 1;
+
+        assertEquals("10->4->1", graph.pathToString(graph.aStar(START, END), graph.getVertex(START)));
     }
 }
